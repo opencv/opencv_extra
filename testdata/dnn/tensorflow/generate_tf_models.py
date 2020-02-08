15e9eddba18733606728526127a3f7ce56865e61
ashishiva3@gmail.com <ashishiva3@gmail.com>
2020-02-08 07:21:58 +0530

    data modified

--- testdata/dnn/tensorflow/generate_tf_models.py
+++ testdata/dnn/tensorflow/generate_tf_models.py
@@ -774,8 +774,8 @@ conv = tf.layers.conv2d(inp, filters=4, kernel_size=[1, 1])
 strided_slice = conv[:, 1:, :2, 2:3]
 save(inp, strided_slice, 'strided_slice')
 ################################################################################
-inp = tf.placeholder(tf.float32, [1, 3, 4, 2], 'input')
-conv_out = tf.keras.layers.Conv2D(filters=2, kernel_size=[3, 3], padding='same')(inp)
+inp = tf.placeholder(tf.float32, [1, 4, 5, 3], 'input')
+conv_out = tf.keras.layers.Conv2D(filters=3, kernel_size=[3, 3], padding='same')(inp)
 crop2d = tf.keras.layers.Cropping2D(((1, 1), (1, 1)))(conv_out)
 save(inp, crop2d, 'crop2d')
 ################################################################################
