719e8ffc1d75ba5bc12c89fc2a67c139c70958e2
czgdp1807 <singh.23@iitj.ac.in>
2020-02-09 14:59:20 +0530

    added files for split

--- testdata/dnn/onnx/generate_onnx_models.py
+++ testdata/dnn/onnx/generate_onnx_models.py
@@ -435,19 +435,22 @@ class Split(nn.Module):
         self.dim = kwargs.get('dim', 0)
 
     def forward(self, x):
-        return torch.split(x, self.split_size_sections, self.dim)
+        output = torch.split(x, self.split_size_sections, self.dim)
+        tensor_o = torch.tensor([output[i].tolist() for i in range(len(output))],
+                                dtype=torch.float32)
+        return tensor_o
 
 model = Split()
-input = Variable(torch.randn([1., 2., 3., 4., 5., 6.]))
+input = Variable(torch.tensor([1., 2., 3., 4., 5., 6.], dtype=torch.float32))
 save_data_and_model("split_1", input, model)
 
 model = Split(dim=0)
 save_data_and_model("split_2", input, model)
 
-model = Split(split_size_sections=[2, 4])
+model = Split(split_size_sections=[3, 3])
 save_data_and_model("split_3", input, model)
 
-model = Split(dim=0, split_size_sections=[2, 4])
+model = Split(dim=0, split_size_sections=[3, 3])
 save_data_and_model("split_4", input, model)
 
 class SplitMax(nn.Module):
