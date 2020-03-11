e8e6c573ecfce07bbe74c829219322f01379590e
Liubov Batanina <piccione-mail@yandex.ru>
2020-03-11 09:19:42 +0300

    Fix input name

--- testdata/dnn/onnx/generate_onnx_models.py
+++ testdata/dnn/onnx/generate_onnx_models.py
@@ -641,4 +641,4 @@ class DynamicResize(nn.Module):
 input_0 = Variable(torch.randn(1, 3, 8, 6))
 input_1 = Variable(torch.randn(1, 3, 4, 3))
 model = DynamicResize()
-save_data_and_model_multy_inputs("dynamic_resize", model, input, input_1, version=11)
+save_data_and_model_multy_inputs("dynamic_resize", model, input_0, input_1, version=11)
