48664d0f9957070e93bae210423066fd17aa4662
Sergey Slashchinin <sergei.slashchinin@xperience.ai>
2020-08-07 02:27:11 +0300

    update link

--- testdata/dnn/download_models.py
+++ testdata/dnn/download_models.py
@@ -866,8 +866,8 @@ models = [
         filename='onnx/models/lightweight_pose_estimation_201912.onnx'),
      Model(
         name='ResNet-tiny',  # https://github.com/opencv/opencv/issues/17588
-        url='https://drive.google.com/uc?export=download&confirm=QccG&id=1Sbu9Is_QemC75goe9vy-fdUwKEU84Pvk',
-        sha='0f8ecd80c6ee7353bdf94042d1dfeb390e21dc59',
+        downloader=GDrive('1Sbu9Is_QemC75goe9vy-fdUwKEU84Pvk'),
+        sha='2fd1f973239525543e3f3975adb1674fbfe69ee6',
         filename='resnet_demo.caffemodel'),
 ]
 
