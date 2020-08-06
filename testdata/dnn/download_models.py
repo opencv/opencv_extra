87f4e366ad61e57a9a20b776c180c0f788206b62
Sergey Slashchinin <sergei.slashchinin@xperience.ai>
2020-08-06 11:16:45 +0300

    Testdata for variable batch size

--- testdata/dnn/download_models.py
+++ testdata/dnn/download_models.py
@@ -853,6 +853,11 @@ models = [
         url='https://github.com/AlexeyAB/darknet/releases/download/darknet_yolo_v4_pre/yolov4-tiny.weights',
         sha='d110379b7b86899226b591ad4affc7115f707157',
         filename='yolov4-tiny.weights'),
+    Model(
+        name='ResNet-tiny',  # https://github.com/opencv/opencv/issues/17588
+        url='https://drive.google.com/uc?export=download&confirm=QccG&id=1Sbu9Is_QemC75goe9vy-fdUwKEU84Pvk',
+        sha='2fd1f973239525543e3f3975adb1674fbfe69ee6',
+        filename='resnet_demo.caffemodel'),
 ]
 
 # Note: models will be downloaded to current working directory
