cf1d384f879eafb86715c04619489d4a47518db7
Julien Maille <julien.maille@gmail.com>
2020-11-17 14:47:15 +0100

    add new test models for Resize layer
    https://github.com/opencv/opencv/issues/18695

--- testdata/dnn/onnx/generate_onnx_models.py
+++ testdata/dnn/onnx/generate_onnx_models.py
@@ -1,6 +1,6 @@
 from __future__ import print_function
 import torch
-from torch.autograd import Variable
+from torch.autograd import Variable, Function
 import torch.nn.init as init
 import torch.nn as nn
 import torch.nn.functional as F
@@ -587,7 +587,7 @@ class ReshapeByDiv(nn.Module):
         channels = image.size(1)
         h = image.size(2)
         w = image.size(3)
-        image = image.view(batch_size, channels*h* (w / 2), -1)
+        image = image.view(batch_size, channels*h*(w // 2), -1)
         return image
 
 input = Variable(torch.randn(1, 2, 3, 4))
@@ -652,7 +652,23 @@ class DynamicResize(nn.Module):
 input_0 = Variable(torch.randn(1, 3, 8, 6))
 input_1 = Variable(torch.randn(1, 3, 4, 3))
 model = DynamicResize()
-save_data_and_model_multy_inputs("dynamic_resize", model, input_0, input_1, version=11)
+save_data_and_model_multy_inputs("dynamic_resize_9", model, input_0, input_1, version=9)
+save_data_and_model_multy_inputs("dynamic_resize_10", model, input_0, input_1, version=10)
+save_data_and_model_multy_inputs("dynamic_resize_11", model, input_0, input_1, version=11)
+save_data_and_model_multy_inputs("dynamic_resize_12", model, input_0, input_1, version=12)
+
+class DynamicResizeScale(nn.Module):
+    def forward(self, x, y):
+        up = nn.Upsample(scale_factor=(0.5, 0.5), mode='bilinear')
+        return up(x) + y
+
+input_0 = Variable(torch.randn(1, 3, 8, 6))
+input_1 = Variable(torch.randn(1, 3, 4, 3))
+model = DynamicResizeScale()
+save_data_and_model_multy_inputs("dynamic_resize_scale_9", model, input_0, input_1, version=9)
+save_data_and_model_multy_inputs("dynamic_resize_scale_10", model, input_0, input_1, version=10)
+save_data_and_model_multy_inputs("dynamic_resize_scale_11", model, input_0, input_1, version=11)
+save_data_and_model_multy_inputs("dynamic_resize_scale_12", model, input_0, input_1, version=12)
 
 class ShapeConst(nn.Module):
     def __init__(self):
@@ -813,7 +829,7 @@ model = UpsampleUnfusedTwoInput()
 save_data_and_model_multy_inputs("upsample_unfused_two_inputs_opset9_torch1.4", UpsampleUnfusedTwoInput(), input_0, input_1, version=9)
 save_data_and_model_multy_inputs("upsample_unfused_two_inputs_opset11_torch1.4", UpsampleUnfusedTwoInput(), input_0, input_1, version=11)
 
- class FrozenBatchNorm2d(nn.Module):
+class FrozenBatchNorm2d(nn.Module):
     def __init__(self, n):
         super(FrozenBatchNorm2d, self).__init__()
         self.register_buffer("weight", torch.ones(n))
@@ -832,7 +848,7 @@ x = Variable(torch.randn(1, 2, 3, 4))
 model = FrozenBatchNorm2d(2)
 save_data_and_model("batch_norm_subgraph", x, model)
 
- class GatherScalar(nn.Module):
+class GatherScalar(nn.Module):
     def forward(self, x):
         return x[1]
 
@@ -840,7 +856,7 @@ x = Variable(torch.randn(2))
 model = GatherScalar()
 save_data_and_model("gather_scalar", x, model)
 
- class Gather(nn.Module):
+class Gather(nn.Module):
     def forward(self, x):
         return x[..., 1]
 
@@ -921,11 +937,11 @@ class MatmulWithTwoInputs(nn.Module):
         const = torch.zeros(1, self.interm_dim)
         const_projected = self.linear_for_const(const)
         const_projected = const_projected.expand(2, self.interm_dim)
-        sum_tanh = torch.tanh(const_projected + x_projected) 
+        sum_tanh = torch.tanh(const_projected + x_projected)
         sum_tanh = sum_tanh.reshape(-1, self.interm_dim)
         sum_tanh_projected = self.second_linear(sum_tanh)
         sum_tanh_projected = sum_tanh_projected.reshape(1, 2)
-        after_softmax = F.softmax(sum_tanh_projected, dim=1)     
+        after_softmax = F.softmax(sum_tanh_projected, dim=1)
         return torch.matmul(after_softmax, x)
 
 x = Variable(torch.rand([1, 2, 2]))
