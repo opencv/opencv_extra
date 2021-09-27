9e3d4272ad73244f8ff618eca49826ef201f90a0
Zihao Mu <zihaomu@outlook.com>
2021-09-27 16:44:50 +0800

    add dim=-2 and 5-dim data.

--- testdata/dnn/tensorflow/generate_tf_models.py
+++ testdata/dnn/tensorflow/generate_tf_models.py
@@ -1034,10 +1034,10 @@ save(inp, sub, prefix + 'bias_add_1', optimize=False)
 ################################################################################
 inp = tf.placeholder(tf.float32, [1, 2, 3, 4], 'input')
 expand_dim = inp + 1
-expand_dim = tf.expand_dims(expand_dim, -1)
+expand_dim = tf.expand_dims(expand_dim, -2)
 save(inp, expand_dim, prefix + 'expand_dim_1', optimize=False)
 ################################################################################
-inp = tf.placeholder(tf.float32, [1, 2, 3, 4], 'input')
+inp = tf.placeholder(tf.float32, [1, 2, 3, 4, 5], 'input')
 expand_dim = inp + 1
 expand_dim = tf.expand_dims(expand_dim, 2)
 save(inp, expand_dim, prefix + 'expand_dim_2', optimize=False)
